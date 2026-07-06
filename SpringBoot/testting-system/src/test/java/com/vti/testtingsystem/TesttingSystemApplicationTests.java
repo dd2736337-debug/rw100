1ff58baf8396285c7dc79e0a8dfc1aea827d7d3b
rw100 <dd2736337@gmail.com>
2026-07-06 19:39:50 +0700

    a

---
+++ SpringBoot/testting-system/src/test/java/com/vti/testtingsystem/TesttingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.testtingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
